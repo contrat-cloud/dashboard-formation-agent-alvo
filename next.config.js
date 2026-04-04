cef6e7c8958063ff9d46165eda458b9df7b725d9
contrat-cloud <contrat@egery.fr>
2026-04-04 04:39:06 +0200

    Add Next.js configuration for security headers

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [{ key: "X-Frame-Options", value: "SAMEORIGIN" }],
+      },
+    ];
+  },
+};
+module.exports = nextConfig;
